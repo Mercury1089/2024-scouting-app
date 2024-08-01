0ffef103a013df4ced8700987b9a3b1a7afd55cc
satsur <satviks.suresh@gmail.com>
2024-07-31 23:18:03 -0400

    it worked now i can actually do things

--- app/src/main/java/com/mercury1089/scoutingapp2024/Auton.java
+++ app/src/main/java/com/mercury1089/scoutingapp2024/Auton.java
@@ -27,7 +27,7 @@ import androidx.fragment.app.Fragment;
 import com.mercury1089.scoutingapp2024.utils.GenUtils;
 
 public class Auton extends Fragment {
-    //HashMaps for sending QR data between screens test
+    //HashMaps for sending QR data between screens
     private LinkedHashMap<String, String> setupHashMap;
     private LinkedHashMap<String, String> autonHashMap;
 
